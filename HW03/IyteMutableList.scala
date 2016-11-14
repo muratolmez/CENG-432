395f60a8e06129fd083a7077d1d37fba9f0daf2f
Murat Olmez <muratolmez@users.noreply.github.com>
2016-11-14 20:10:21 +0200

    Create IyteMutableList.scala

---
+++ HW03/IyteMutableList.scala
@@ -0,0 +1,48 @@
+class IyteMutableList{
+
+
+	class Node(var data : Int, var next: Node)
+	private var head:Node = null
+	private var tail:Node = null
+
+
+	def add(data :Int):Unit={
+
+		if(head != null){
+
+			tail.next = new Node(data,null)
+			tail = tail.next
+
+		}
+
+		else{
+
+			head = new Node(data,null)
+			tail=head
+		}
+	}
+
+override def toString() : String = {
+
+	var result:String = ""
+	var flag = head
+	if(flag == null)
+		return ""
+
+	while(flag != null){
+		result += flag.data
+		result += ","
+		flag = flag.next
+	}
+	return result.substring(0,result.length-1)
+}
+
+
+	
+
+}
+
+object IyteMutableList {
+	
+	def apply : IyteMutableList = new IyteMutableList()
+}
